0291c288ce86a95b12ee37d0f5ec044eab50b710
NikitaKolotushkin <nikitaplay1000@gmail.com>
2022-01-15 22:53:01 +0400

    Заготовка для управления

--- main.py
+++ main.py
@@ -3,16 +3,20 @@
 
 import pygame as pg
 
+from Player import Player
 from tools import load_image
 from tools import *
 
 
 class App:
+    """
+    App class
+    """
 
     def __init__(self):
         self.screen = pg.display.set_mode(SIZE)
         self.clock = pg.time.Clock()
-        self.fps = 60
+        self.fps = 30
         pg.display.set_caption('Pac-Man')
 
     def run(self):
@@ -20,6 +24,15 @@ class App:
             for e in pg.event.get():
                 if e.type == pg.QUIT:
                     exit()
+                if e.type == pg.KEYDOWN:
+                    if e.key == pg.K_LEFT or e.key == pg.K_a:
+                        pass
+                    if e.key == pg.K_RIGHT or e.key == pg.K_d:
+                        pass
+                    if e.key == pg.K_UP or e.key == pg.K_w:
+                        pass
+                    if e.key == pg.K_DOWN or e.key == pg.K_s:
+                        pass
             
             pg.display.flip()
             self.clock.tick(self.fps)
